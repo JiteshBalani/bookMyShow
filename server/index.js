fb896ab84bd930cc97ba6e64185f08ef061ccb90
JiteshBalani <balani.jitesh911@gmail.com>
2025-02-16 20:45:44 +0530

    Backend - Changed origin of cors to vercel link.

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://topshow.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // If you're using cookies/sessions
